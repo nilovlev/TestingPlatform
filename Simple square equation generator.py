01efcfdb0b7696993aef026ab686ee7654c306eb
nilovlev <nilovlev@yandex.ru>
2021-02-09 19:40:59 +0300

    add Simple square equation generator

---
+++ Simple square equation generator.py
@@ -0,0 +1,25 @@
+import random
+
+
+class SquareEquationGenerator:
+    """Класс генерации квадратных уравнений"""
+
+    @staticmethod
+    def generate(count: int):
+        """
+        Функция для генерации квадратных уравнений
+        :param count: Количество сгенерированных уравнений
+        :return: Функция возвращает словарь типа {<уравнение>: {'x1': <x1>, 'x2': <x2>}}
+        """
+        eq_list = []
+        for eq in range(count):
+            x1, x2 = -1 * random.randint(-10, 10), -1 * random.randint(-10, 10)
+            eq_list.append({'x^2 {0}{1}x {2}{3} = {4}'.format(('+ ' if x1 + x2 >= 0 else '- '),
+                                                              str(abs((x1 + x2))),
+                                                              ('+ ' if x1 * x2 >= 0 else '- '), str(abs(x1 * x2)),
+                                                              str(0)): {'x1': -1 * x1, 'x2': -1 * x2}})
+        return eq_list
+
+
+for eq in SquareEquationGenerator.generate(5):
+    print(eq)
